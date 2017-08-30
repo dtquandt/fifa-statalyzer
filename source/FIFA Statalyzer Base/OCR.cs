c504c28985fd2d13641a5aac5532250c91d88c3e
Daniel Quandt <usquandts@gmail.com>
2017-08-29 21:36:47 -0300

    Separated classes into their own files, implemented first version of OCR, rearranged some files

---
+++ source/FIFA Statalyzer Base/OCR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Tesseract;
+using System.IO;
+
+public class OCR
+{
+	public static string ReadImage(string imagepath)
+	{
+        using (var ocrEngine = new TesseractEngine(@".\tessdata", @"eng", EngineMode.Default))
+        {
+            using (var image = Pix.LoadFromFile(imagepath))
+            {
+                using (var result = ocrEngine.Process(image,Tesseract.PageSegMode.SingleBlock))
+                {
+                    var text = result.GetText();
+                    return text;
+                }
+            }
+        }
+    }
+}
---
+++ source/FIFA Statalyzer Base/dbManagement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data.SQLite;
+
+public class DBManagement
+{
+
+    public static SQLiteConnection InitializeDB()
+    {
+        SQLiteConnection dbConnection;
+        if (File.Exists("FifaStatalyzer.sqlite") == false)
+        {
+            SQLiteConnection.CreateFile("FifaStatalyzer.sqlite");
+        }
+        dbConnection = new SQLiteConnection("Data Source =FifaStatalyzer.sqlite;Version=3;");
+        dbConnection.Open();
+        return dbConnection;
+    }
+
+    public static void ExecuteNonQuery(string command, SQLiteConnection dbConnection)
+    {
+        SQLiteCommand sqlcommand = new SQLiteCommand(command, dbConnection);
+        sqlcommand.ExecuteNonQuery();
+    }
+}
+
+
+
+//    DBManagement.ExecuteNonQuery("CREATE TABLE if not exists 'fifa' ('pscore' INT, 'ppos' INT, 'opscore' INT, 'oppos' INT, 'result' INT);", dbConnection);
+//            // 0 = loss, 1 = draw, 2 = win
+//            int result = 0;
+//    Console.WriteLine("What was your score?");
+//            int.TryParse(Console.ReadLine(), out int pscore);
+//    Console.WriteLine("What was the opponent's score?");
+//            int.TryParse(Console.ReadLine(), out int opscore);
+//    Console.WriteLine("What was your ball possession?");
+//            int.TryParse(Console.ReadLine(), out int ppos);
+//    //  Console.WriteLine("What was your opponent's ball possession?");
+//    //  int.TryParse(Console.ReadLine(), out int oppos);
+//    int oppos = (100 - ppos);
+//            if (opscore > pscore) { result = 0; }
+//            if (opscore == pscore) { result = 1; }
+//            if (opscore<pscore) { result = 2; }
+//            sql = "insert into fifa (pscore, opscore, ppos, oppos, result) values ("+ pscore + ", " + opscore + ", " + ppos + ", " + oppos + ", " + result + ");";
+//            sqlcommand = new SQLiteCommand(sql, dbConnection);
+//  sqlcommand.ExecuteNonQuery();
+
