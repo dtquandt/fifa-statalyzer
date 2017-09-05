35d7bb3b07e35fec10e3c839706167656eeba45f
Daniel Quandt <usquandts@gmail.com>
2017-09-05 19:00:19 -0300

    Added regex processing of OCR output

--- source/FIFA Statalyzer Base/Program.cs
+++ source/FIFA Statalyzer Base/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
 using System.Data.SQLite;
@@ -16,7 +17,7 @@ namespace FIFA_Statalyzer_Base
         static void Main(string[] args)
         {
 #if DEBUG
-            args = new[] { @"C:\test" };
+            args = new[] { @"C:\test\active" };
 #endif
             string folderPath = args[0];
             string[] fileList = Directory.GetFiles(folderPath, "*.png");
@@ -24,8 +25,17 @@ namespace FIFA_Statalyzer_Base
             foreach(string file in fileList)
             {
                 string ocr = (OCR.ReadImage(file));
-                Console.ReadLine();
+                ocr = ocr.Replace("\n", " ");
+                ocr = Regex.Replace(ocr, "[^0-9 ]", "");
+                ocr = Regex.Replace(ocr, @"\s+", " ");
+                string[] values = ocr.Split(' ');
+                Console.WriteLine(ocr);
+                foreach (string value in values)
+                {
+                    Console.WriteLine(value);
+                }
             }
+            Console.ReadLine();
         }
     }
 }
