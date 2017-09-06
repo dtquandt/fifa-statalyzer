bfee0cad6e2eabd0d35f832cc1977624214e3318
Daniel Quandt <usquandts@gmail.com>
2017-09-06 16:15:27 -0300

    Implemented database entry for OCR results

--- source/FIFA Statalyzer Base/Program.cs
+++ source/FIFA Statalyzer Base/Program.cs
@@ -25,8 +25,9 @@ namespace FIFA_Statalyzer_Base
                 args = new[] { Directory.GetCurrentDirectory() + @"\images\processed\" };
             }
             //Sets working path
-            string folderPath = args[0];
+            string folderPath = args[0]; 
             string[] fileList = Directory.GetFiles(folderPath, "*.png");
+            SQLiteConnection dbConnection = DBManagement.InitializeDB();
             foreach (string file in fileList)
             {
                 Dictionary<string, int> statsDict = new Dictionary<string, int>();
@@ -37,10 +38,33 @@ namespace FIFA_Statalyzer_Base
                 for (int i = 0; i < ocrValues.Length; i++)
                 {
                     statsDict.Add(legend[i], (int.Parse(ocrValues[i])));
-                    Console.WriteLine(legend[i] + " " + statsDict[legend[i]]);
                 }
-                SQLiteConnection dbconnection = DBManagement.InitializeDB();
-
+                int result = 0;
+                statsDict.TryGetValue("hGoals", out int hGoals);
+                statsDict.TryGetValue("aGoals", out int aGoals);
+                if (hGoals == aGoals) { result = 0; } // Draw
+                else if (hGoals > aGoals) { result = 1; } // Home win
+                else if (hGoals < aGoals) { result = 2; } // Away win
+                statsDict.Add("result", result);
+                foreach (var pair in statsDict)
+                {
+                    Console.WriteLine("{0}: {1}", pair.Key, pair.Value);
+                }
+                StringBuilder commandBuilder = new StringBuilder("insert into fifa(");
+                foreach (var pair in statsDict)
+                {
+                    commandBuilder.Append(pair.Key + ", ");
+                }
+                commandBuilder.Length -= 2;
+                commandBuilder.Append(") values (");
+                foreach (var pair in statsDict)
+                {
+                    commandBuilder.Append(pair.Value + ", ");
+                }
+                commandBuilder.Length -= 2;
+                commandBuilder.Append(");");
+                string sqlCommand = commandBuilder.ToString();
+                DBManagement.ExecuteNonQuery(sqlCommand, dbConnection);
             }
             Console.ReadLine();
         }
